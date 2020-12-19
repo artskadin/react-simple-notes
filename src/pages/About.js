58f2acebd2970b7b5cdc94b2b1cb80f69b362cf6
Artem <artskadin@gmail.com>
2020-12-19 20:13:01 +0300

    realise app v1.0

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Тренирую навыки React</h1>
+            <p className="lead">
+                Пишу менеджер задач на реакт хуках. Версия приложения <strong>1.0.13</strong>
+            </p>
+        </div>
+    </div>
+)
